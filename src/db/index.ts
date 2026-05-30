93412cffd631bce62ba829de1b2f990566e8debd
Shadrr <purwasadr@gmail.com>
2026-05-30 16:27:35 +0700

    chore: fix some

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,14 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from './schema';
-import { relations } from './schema/relation';
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+
+import { relations } from './schema/relation'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+})
 
 export const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-  },
-  schema: schema,
+  client: pool,
   relations: relations,
 })
